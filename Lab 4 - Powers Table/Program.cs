9c3fd3a96a1299574bd9910c7866a14e6e70931a
Erwin <coronele@hotmail.com>
2020-01-24 09:49:12 -0500

    Final version with documentation

--- Lab 4 - Powers Table/Program.cs
+++ Lab 4 - Powers Table/Program.cs
@@ -8,12 +8,18 @@ namespace Lab_4___Powers_Table
         {
             string continueEntry;
 
+            // This shows the program title
             ShowTitle();
 
             do
             {
+                // Prompt for and receive input
                 int userInt = GetUserInt("We will need an integer to square/cube.\n");
+                
+                // Display powers table
                 ShowPowersTable(userInt);
+                
+                // Prompt to repeat program
                 continueEntry = TryAgain();
             }
             while (continueEntry == "y");
@@ -29,11 +35,15 @@ namespace Lab_4___Powers_Table
 
         public static int GetUserInt(string message)
         {
+            
+            // Gets integer for user to process into squares and cubes.  Includes validation.
             SetOutputColor();
             Console.WriteLine(message);
             
             int input;
             
+            // input validation
+            // Will repeat until valid int is entered.  Will return input (which contains entered int) if input valid.
             while (!(int.TryParse(message, out input)))
             {
                 SetOutputColor();
@@ -45,18 +55,21 @@ namespace Lab_4___Powers_Table
         }
         public static void SetInputColor()
         {
+            // Method for setting colors for user inputted text
             Console.ForegroundColor = ConsoleColor.DarkGray;
             Console.BackgroundColor = ConsoleColor.Black;
         }
 
         public static void SetOutputColor()
         {
+            // Method for setting colors for default display text
             Console.ForegroundColor = ConsoleColor.White;
             Console.BackgroundColor = ConsoleColor.Black;
         }
 
         public static void ShowTitle()
         {
+            // This method simply shows the title
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.BackgroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("Powers Table\n\n");
@@ -65,6 +78,7 @@ namespace Lab_4___Powers_Table
 
         public static void ShowPowersTable(int UserInt)
         {
+            // Prints the powers table from 1 to user entered number
             SetOutputColor();
 
             Console.WriteLine("\n\n  {0,15}      {1,15}       {2,15}", "number", "square", "cube");
@@ -77,6 +91,7 @@ namespace Lab_4___Powers_Table
 
         public static string TryAgain()
         {
+            // Method for running program again.  Passes back to do while loop in main.
             string userChoice = GetUserInput("Would you like to run again? [y/n] ").ToLower();
             while ((userChoice!="y") && (userChoice!="n"))
             {
@@ -87,11 +102,13 @@ namespace Lab_4___Powers_Table
 
         public static int Square(int num)
         {
+            // squares value
             return num * num;
         }
 
         public static int Cube(int num)
         {
+            // cubes value
             return num * num * num;
         }
     }
